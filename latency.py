978bf23719e5d69d03f0951008baab3883cf2901
traianuspartners <132686768+traianuspartners@users.noreply.github.com>
2024-02-22 17:11:00 +0100

    Update latency.py

--- latency.py
+++ latency.py
@@ -1,42 +1,63 @@
-import ccxt
-import time
-import logging
+# -*- coding: utf-8 -*-
 
-# Configurazione del logger per registrare in nanosecondi
-logging.basicConfig(filename='latency_log_ns.txt', level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+import os
+import sys
 
-api_key = 'YOUR_API_KEY'
-secret = 'YOUR_API_SECRET'
-bybit = ccxt.bybit({
-    'apiKey': api_key,
-    'secret': secret,
-    'enableRateLimit': True,
-})
 
-symbol = 'BTC/USDT:USDT'
-amount = 0.001
-price = 10000
+import ccxt  # noqa: E402
 
-try:
-    # Inizio misurazione latenza per la creazione dell'ordine
-    start_ns = time.perf_counter_ns()
-    order = bybit.create_limit_buy_order(symbol, amount, price)
-    end_ns = time.perf_counter_ns()
-    latency_creation_ns = end_ns - start_ns
-    print(f"Ordine creato in {latency_creation_ns} nanosecondi")
-    logging.info(f"Order created. Latency: {latency_creation_ns} ns. Order details: {order}")
 
-    # Inizio misurazione latenza per la cancellazione dell'ordine
-    start_cancel_ns = time.perf_counter_ns()
-    bybit.cancel_order(order['id'], symbol)
-    end_cancel_ns = time.perf_counter_ns()
-    latency_cancel_ns = end_cancel_ns - start_cancel_ns
-    print(f"Ordine cancellato in {latency_cancel_ns} nanosecondi")
-    logging.info(f"Order cancelled. Latency: {latency_cancel_ns} ns.")
-except ccxt.BaseError as e:
-    logging.error(f"CCXT Error: {e}")
-    print(f"Errore CCXT: {e}")
-except Exception as e:
-    logging.error(f"General Error: {e}")
-    print(f"Errore Generico: {e}")
+def print_supported_exchanges():
+   print('Supported exchanges:')
+   print(ccxt.exchanges)
+
+
+def print_usage():
+   print('Usage: python ', sys.argv[0], 'id', 'num_orders', 'symbol', 'side', 'amount', 'price')
+   print_supported_exchanges()
+
+def main():
+   try:
+       id = sys.argv[1]  # get exchange id from command line arguments
+       # check if the exchange is supported by ccxt
+       if id in ccxt.exchanges:
+           print('Instantiating', id)
+           # instantiate the exchange by id
+           exchange = getattr(ccxt, id)({
+               'apiKey': 'YOUR_API_KEY',
+               'secret': 'YOUR_SECRET',
+               # you have to disable the rate limiter to measure the real latency
+               'enableRateLimit': False,
+           })
+           print('Loading', id, 'markets')
+           # load all markets from the exchange
+           markets = exchange.load_markets()
+           print('Loaded', id, 'markets')
+           num_orders = int(sys.argv[2])
+           symbol = sys.argv[3]
+           side = sys.argv[4]
+           amount = float(sys.argv[5])
+           price = float(sys.argv[6])
+           if symbol in markets:
+               print('Testing', symbol)
+               results = []
+               for i in range(0, num_orders):
+                   timestamp = exchange.milliseconds()
+                   order = exchange.create_order(symbol, 'limit', side, amount, price)
+                   elapsed = exchange.milliseconds() - timestamp
+                   canceled = exchange.cancel_order(order['id'], symbol)
+                   results.append(elapsed)
+                   print(elapsed, 'ms')
+               sum = exchange.sum(*results)
+               average = int(sum / len(results))
+               print('Average:', average, 'ms')
+       else:
+           print('Exchange', id, ' not found')
+           print_usage()
+   except Exception as e:
+       print(type(e).__name__, str(e))
+       print_usage()
+
+print('CCXT Version:', ccxt.__version__)
+main()
+
